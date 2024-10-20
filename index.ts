2bbc4bea747a977cf0d7139a7b35d675d1305ea3
Robetjunior <juniorferreirarob@gmail.com>
2024-10-19 22:48:33 -0300

    update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors'; // Importar CORS
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import { sequelize } from './models';
 import invoiceRoutes from './src/routes/invoice';
@@ -10,30 +10,32 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Opções do CORS para permitir tudo o que o front pode precisar
+// Configurações de CORS para permitir múltiplas origens e credenciais
+const allowedOrigins = ['https://lumi-front-82af2d71e234.herokuapp.com', 'https://outra-origem-permitida.com'];
 const corsOptions = {
-  origin: ['https://lumi-front-82af2d71e234.herokuapp.com'], // Adicione mais origens aqui se necessário
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Permitir todos os métodos HTTP
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Origin', 'Accept'], // Permitir cabeçalhos mais comuns
+  origin: (origin: any, callback: any) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origem não permitida pelo CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Origin', 'Accept'],
   credentials: true, // Permitir o uso de credenciais (cookies, tokens de autenticação)
-  preflightContinue: false, // Preflight não precisa de resposta personalizada
-  optionsSuccessStatus: 204, // Para evitar problemas com o status em alguns navegadores
+  optionsSuccessStatus: 200,
 };
 
-// Aplicar CORS globalmente com as opções configuradas
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Habilita preflight para todas as rotas
 
-// Tratativa adicional para requisições OPTIONS
-app.options('*', cors(corsOptions));
-
-// Parsing de JSON e URL-encoded
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Rotas
 app.use('/api/invoices', invoiceRoutes);
 
-// Conexão com banco de dados
+// Conexão com o banco de dados
 sequelize.authenticate()
   .then(() => {
     console.log('Conexão com o banco de dados foi bem-sucedida!');
@@ -42,7 +44,6 @@ sequelize.authenticate()
     console.error('Erro ao conectar ao banco de dados:', err);
   });
 
-// Iniciar o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
